1b761b6af0963f107dca7a62cea3ab18d379ba7e
kqian5 <kycqian1005@gmail.com>
2018-07-01 20:26:29 -0400

    JRCP Documentation

--- src/main/java/common/db/JobRecordConnectionProvider.java
+++ src/main/java/common/db/JobRecordConnectionProvider.java
@@ -4,16 +4,29 @@ import edu.gsu.dmlab.isd.monitor.IJobRecordConnection;
 import edu.gsu.dmlab.isd.monitor.PostgresJobRecordConnection;
 
 /**
- * Created by ahmetkucuk on 3/10/18.
+ * Class that connects to the postgre database
+ * @author - ahmetkucuk
+ * @author - kqian5
  */
 public class JobRecordConnectionProvider {
     private static JobRecordConnectionProvider instance;
     private PostgresJobRecordConnection monitorConnection;
 
+    /**
+     * Constructor for a JobRecordProvider object that
+     * establishes a connection to the postgre database
+     * using configurations from DBPrefs
+     */
     private JobRecordConnectionProvider() {
         monitorConnection = new PostgresJobRecordConnection(DBPrefs.getDataSource());
     }
 
+    /**
+     * Returns the connection object from a new instance of 
+     * a JobRecordConnectionProvider object
+     * @return - a PostgresJobRecordConnection object from 
+     * a new instance of a JobRecordConnectionProvider object
+     */
     public static IJobRecordConnection connection() {
         if (instance == null) {
             instance = new JobRecordConnectionProvider();
